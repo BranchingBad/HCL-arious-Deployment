8c47c316753ac6089d93ea7fa822fcdae9e9406c
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2025-11-06 18:36:42 -0500

    update outputs

--- container-App-GKE/outputs.tf
+++ container-App-GKE/outputs.tf
@@ -1,21 +1,15 @@
 output "cluster_name" {
-  description = "The name of the GKE cluster."
+  description = "GKE Cluster Name"
   value       = google_container_cluster.primary.name
 }
 
 output "cluster_endpoint" {
-  description = "The PRIVATE endpoint of the GKE cluster's master."
+  description = "The public endpoint of the GKE cluster."
   value       = google_container_cluster.primary.endpoint
   sensitive   = true
 }
 
-output "bastion_ssh_command" {
-  description = "Command to SSH into the bastion host via IAP."
-  value       = "gcloud compute ssh ${google_compute_instance.bastion.name} --zone ${google_compute_instance.bastion.zone} --tunnel-through-iap --project ${var.project_id}"
-}
-
-output "kubeconfig" {
-  description = "Command to configure kubectl (MUST be run on the Bastion host)."
-  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${google_container_cluster.primary.location} --project ${var.project_id} --internal-ip"
-  sensitive   = true
+output "get_credentials_command" {
+  description = "Command to configure kubectl."
+  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project_id}"
 }
